bab5639c1c0b7c2fc46ca919a410dab829cce0be
Bely-Z <62282571+Bely-Z@users.noreply.github.com>
2023-06-29 11:59:39 +0800

    Bely‘s Portfolio

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Bely-Zhao-ab2d599e2177403b88ddb8288e880561',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Bely's Portfolio',
+  domain: 'https://www.notion.so/Bely-Zhao-ab2d599e2177403b88ddb8288e880561',
+  author: 'Bely',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
